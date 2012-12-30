e98b1591e7de5f0d2c7a254070aae538400888b2
Najeeb <ns.jeeb@gmail.com>
2012-12-30 14:25:06 -0500

    text added to index and services page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -5,5 +5,13 @@ $this->pageTitle=Yii::app()->name;
 ?>
 <div>
 	<div class="text_content"><img src="<?php echo Yii::app()->request->baseUrl;?>/images/itemImages/header_nonferrous_scrap_03.png"  alt="copper scrap aluminum metal scrap" /></div>
-	<div class="text_content">right sid div</div>
+	<div class="text_content"><h3>Eagle Ontario Iron &amp; Metal</h3>
+		<ul>
+		<li>Over 10 years on recycling of Ferrous and Non Ferrous Metals</li>
+		<li>Thinks Green and Environmentally Friendly</li>
+		<li>We Buy:</li></ul> 
+			<ul><li> iron, steel, stainless steel, brass, copper, aluminum, zinc, nickel, and lead</li></ul>
+		
+		
+	</div>
 </div>
--- protected/views/site/services.php
+++ protected/views/site/services.php
@@ -1,2 +1,17 @@
 <?php
-echo 'Service';
+
+?>
+<div>
+	<div class="text_content">
+		
+	</div>
+	
+	<div class="text_content"><h3>Services</h3>
+		<p>
+		For over 10 years Eagle Ontario Iron &amp; Metal has been serving Toronto &#39;s community to separate recycling ferrous and nonferrous metals.
+		We are located in the center of Scarborough, and we are specializes in the purchasing, ranking, buying and selling of all 
+		ferrous and non-ferrous scrap metals from the manufacturing, dealer and service industries.
+		</p>
+	</div>
+</div>
+
