59b3e38da8d649c4e676258b9b4910dd39c05db6
Pirdad Sakhizada <pirdod@gmail.com>
2012-12-31 14:41:37 -0500

    Created the admin item page and finished the log in part of the admin.

--- protected/views/admin/items.php
+++ protected/views/admin/items.php
@@ -1,3 +1,19 @@
-<?php ?>
+<?php
 
+$dataProvider = new CActiveDataProvider('Item');
 
+$this->widget('zii.widgets.grid.CGridView', array(
+    'dataProvider'=>$dataProvider,
+ 	'columns'=>array(
+        'title',          // display the 'title' attribute
+		'picture',
+		'description',
+		'link',
+        array(            // display a column with "view", "update" and "delete" buttons
+            'class'=>'CButtonColumn',
+        	'template'=>'{update}{delete}'
+        ),
+    ),
+));
+
+?>
--- protected/views/site/items.php
+++ protected/views/site/items.php
@@ -1,11 +1,11 @@
 <?php
 // $items should have lla the images form database and diplay in the follwing table....
-$itme;
+$item;
 	echo "<table><th>Name</th><th>Description</th><th>Size</th><tr>";
 	for ($i =0; $i<3; $i++)
 	{
 		//echo"<td>$items[$i]->itme</td>";
-		echo "<td>".$itme->title."</td>";
+		//echo "<td>".$item->title."</td>";
 	}
 	echo "</tr><table>";
 ?>
