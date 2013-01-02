095e6db3f477a8dd1862769f18042388ac58b27b
Pirdad Sakhizada <pirdod@gmail.com>
2013-01-01 19:29:21 -0500

    finished all the admin stuff.

--- protected/views/admin/items.php
+++ protected/views/admin/items.php
@@ -1,12 +1,19 @@
 <?php
 
 $dataProvider = new CActiveDataProvider('Item');
+$base_url = Yii::app()->baseurl;
+echo "<a href='$base_url./index.php?r=admin/insert' />Insert New Item</a>";
 
 $this->widget('zii.widgets.grid.CGridView', array(
     'dataProvider'=>$dataProvider,
  	'columns'=>array(
         'title',          // display the 'title' attribute
-		'picture',
+		array(
+            'name'=>'picture',
+            'type'=>'html',
+            'value'=>'CHtml::image($data->picture, $data->title, array("style"=>"height:45px;"))',
+        ),
+        'picture',
 		'description',
 		'link',
         array(            // display a column with "view", "update" and "delete" buttons
