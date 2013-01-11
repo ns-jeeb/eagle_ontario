4e79641c7425884fe557d421777cd06aed7cd8fb
Najeeb <ns.jeeb@gmail.com>
2013-01-11 13:04:43 -0500

    resize the image

--- protected/views/site/items.php
+++ protected/views/site/items.php
@@ -2,7 +2,7 @@
 // $items should have lla the images form database and diplay in the follwing table....
 
 //print_r($item);
-	echo "<table><tr><th>Name</th><th>Description</th><th>Picture</th><th>Link</th></tr>";
+	echo "<table><tr><th>Name</th><th>Picture</th><th>Description</th><th>Link</th></tr>";
 	for ($i =0; $i<count($item); $i++)
 	{
 		echo "<tr>";
@@ -11,8 +11,8 @@
 			for ($td =0; $td< 1; $td++)
 			{
 				echo "<td>".$item[$i]->title."</td>";
+				echo "<td><img src='".$item[$i]->picture."' alt='scrap' style='width: 150px; background: red;' /></td>";
 				echo "<td>".$item[$i]->description."</td>";
-				echo "<td><img src='".$item[$i]->picture."' alt='scrap'/></td>";
 				echo "<td>".$item[$i]->link."</td>";
 			}
 				
@@ -25,4 +25,3 @@
 	}
 	echo "</table>";
 ?>
-
